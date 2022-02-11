3d32a2dc1aed085aad66262760c148e28fdf6cad
TechnicJelle <22576047+TechnicJelle@users.noreply.github.com>
2022-02-11 14:54:21 +0100

    Serial port fixed and implemented

--- Game/MyGame/MyGame/Gamepad.cs
+++ Game/MyGame/MyGame/Gamepad.cs
@@ -1,6 +1,5 @@
-﻿using System;
-using System.IO.Ports;
-using System.Threading;
+﻿using System.IO.Ports;
+using System.Linq;
 using GXPEngine;
 using GXPEngine.Core;
 
@@ -8,33 +7,34 @@ namespace MyGame.MyGame;
 
 public static class Gamepad
 {
-    private static SerialPort _serialPort;
+    private static readonly SerialPort SerialPort;
 
 	private const float DEAD_ZONE = 0.2f;
-	private const float DIST = 150f;
 
 	public static Vector2 _joystick { get; private set; }
 
 	static Gamepad()
 	{
-		_serialPort = new SerialPort();
-		_serialPort.PortName = "COM6"; //Set your board COM
-		_serialPort.BaudRate = 9600;
-		_serialPort.Open();
+		SerialPort = new SerialPort();
+		SerialPort.PortName = "COM3"; //Set your board COM
+		SerialPort.BaudRate = 9600;
+		SerialPort.Open();
 
 		_joystick = new Vector2(0, 0);
 	}
 
 	public static void Update()
 	{
-		string val = _serialPort.ReadExisting().Trim();
-		Console.WriteLine(val);
-		int[] values = Array.ConvertAll(val.Split(','), int.Parse);
-		Console.WriteLine(values);
-		float x = Mathf.Map(values[0], 0, 1023, -DIST, DIST);
-		if (x < DEAD_ZONE) x = 0.0f;
-		float y = Mathf.Map(values[1], 0, 1023, -DIST, DIST);
-		if (y < DEAD_ZONE) y = 0.0f;
-		_joystick = new Vector2(x, y).Limit(1);
+		string val = SerialPort.ReadExisting().Trim();
+		string[] lines = val.Split('\n');
+		string[] split = lines.Last().Split(',');
+		if (split.Length != 3) return;
+		int valX = int.Parse(split[0].Trim());
+		int valY = int.Parse(split[1].Trim());
+		float x = Mathf.Map(valX, 0, 1023, -1.0f, 1.0f);
+		float y = Mathf.Map(valY, 0, 1023, -1.0f, 1.0f);
+		if (Mathf.Abs(x) < DEAD_ZONE) x = 0.0f;
+		if (Mathf.Abs(y) < DEAD_ZONE) y = 0.0f;
+		_joystick = new Vector2(x, y).Limit(1.0f);
 	}
 }
