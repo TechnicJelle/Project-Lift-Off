0a32b23928d89bc370e1ab4fcc9fedf6d1bdc31d
TechnicJelle <22576047+TechnicJelle@users.noreply.github.com>
2022-03-03 14:54:11 +0100

    Gamepad fix

--- Game/MyGame/MyGame/Gamepad.cs
+++ Game/MyGame/MyGame/Gamepad.cs
@@ -9,6 +9,7 @@ namespace MyGame.MyGame;
 public static class Gamepad
 {
 	private const float DEAD_ZONE = 0.2f;
+	private const float TOP_ZONE = 0.98f;
 
     private static readonly SerialPort SerialPort;
     public static Vector2 _joystick { get; private set; }
@@ -22,6 +23,7 @@ public static class Gamepad
 			SerialPort.BaudRate = 9600;
 			SerialPort.Open();
 			SerialPort.DataReceived += DataReceivedHandler;
+			Console.WriteLine("Found gamepad!");
 		}
 		catch (Exception)
 		{
@@ -34,27 +36,27 @@ public static class Gamepad
 
 	private static void DataReceivedHandler(object sender, SerialDataReceivedEventArgs e)
 	{
-		float x = 0.0f;
-		float y = 0.0f;
 
 		SerialPort sp = (SerialPort)sender;
-		// Console.WriteLine(sp.ReadLine());
 
-		if (sp != null)
-		{
-			string val = sp.ReadLine().Trim();
-			string[] lines = val.Split('\n');
-			string[] split = lines.Last().Split(',');
-			if (split.Length != 5) return;
-			int valX = int.Parse(split[0].Trim());
-			int valY = int.Parse(split[1].Trim());
-			x = Mathf.Map(valX, 0, 1023, -1.0f, 1.0f);
-			y = Mathf.Map(valY, 0, 1023, -1.0f, 1.0f);
-			Console.WriteLine(x +", "+ y);
-			if (Mathf.Abs(x) < DEAD_ZONE) x = 0.0f;
-			if (Mathf.Abs(y) < DEAD_ZONE) y = 0.0f;
-		}
+		if (sp == null) return;
+
+		string val = sp.ReadLine().Trim();
+		string[] lines = val.Split('\n');
+		string[] split = lines.Last().Split(',');
+		if (split.Length != 3) return;
+		int valX = int.Parse(split[0].Trim());
+		int valY = int.Parse(split[1].Trim());
+		float x = Mathf.Map(valX, 0, 1023, -1.0f, 1.0f);
+		float y = Mathf.Map(valY, 0, 1023, -1.0f, 1.0f);
+		if (Mathf.Abs(x) < DEAD_ZONE) x = 0.0f;
+		if (Mathf.Abs(y) < DEAD_ZONE) y = 0.0f;
+		if (x > TOP_ZONE) x = 1.0f;
+		if (x < -TOP_ZONE) x = -1.0f;
+		if (y > TOP_ZONE) y = 1.0f;
+		if (y < -TOP_ZONE) y = -1.0f;
 		_joystick = new Vector2(x, y).Limit(1.0f);
+		// Console.WriteLine(_joystick.ToString());
 	}
 
 	public static void Update()
